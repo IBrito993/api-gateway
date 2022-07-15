53b39265125172b0d1edb0bb2a9945e20a3c2090
IBrito993 <ivanbri93@gmail.com>
2022-07-15 15:18:01 -0400

    First Commit

---
+++ src/test/java/com/ibrito/microservices/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibrito.microservices.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
