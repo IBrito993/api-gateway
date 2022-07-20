b507f0b0f03f37702fec1c7cf3e6dabe316ff7dd
IBrito993 <ivanbri93@gmail.com>
2022-07-20 12:07:15 -0400

    First Commit

---
+++ src/main/java/com/ibrito/microservices/apigateway/config/ApiGatewayConfiguration.java
@@ -0,0 +1,24 @@
+package com.ibrito.microservices.apigateway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ApiGatewayConfiguration {
+
+    @Bean
+    public RouteLocator gatewayRouter(RouteLocatorBuilder routeLocatorBuilder) {
+
+        return routeLocatorBuilder.routes()
+                .route(p -> p.path("/get")
+                        .filters(f -> f.addRequestHeader("MyHeader", "MyURI").addRequestParameter("Parameter", "Parameter"))
+                        .uri("http://httpbin.org:80"))
+                .route(p -> p.path("/currency-exchange/**")
+                        .uri("lb://currency-exchange"))
+                .route(p -> p.path("/currency-conversion/**")
+                        .uri("lb://currency-conversion"))
+                .build();
+    }
+}
